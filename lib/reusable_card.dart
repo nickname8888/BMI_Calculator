cd7975610b7ce7d0e6b37ecc6d589c2348d6df98
Neeraj <neeraj.nerkar@gmail.com>
2021-07-23 23:33:28 +0530

    added 2 new dart files, refactored code and added icons

---
+++ lib/reusable_card.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  late final Color colour;
+  late final Widget cardChild;
+
+  ReusableCard({required this.colour, required this.cardChild});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
